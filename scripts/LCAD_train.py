25b25023ea6cec893642d2d2089037c73ba91b5f
andersonmozart <andersonmozart75@gmail.com>
2019-12-10 15:34:03 -0300

    Script treino

--- scripts/LCAD_train.py
+++ scripts/LCAD_train.py
@@ -3,7 +3,7 @@ import os, sys
 
 def train_model(txt_rddf, image_folder):
     dados_rddf = []
-    if(image_folder[-1] == '/'):
+    if('/' == image_folder[-1]):
         image_folder = image_folder[:-1]
     with open(txt_rddf, 'r') as f:
         dados_rddf = [line.strip().split(" ") for line in f]
@@ -11,6 +11,11 @@ def train_model(txt_rddf, image_folder):
     for i in range(len(dados_rddf)):
         if(False == os.path.isfile(image_folder + '/' + dados_rddf[i][5] + '-r.png')):
             print(image_folder + '/' + dados_rddf[i][5] + '-r.png')
+    
+    print(dados_rddf)
 
 #train_model("/dados/rddf_predict/te.txt", "/dados/log_png_1003")
-train_model(sys.argv[1], sys.argv[2])
+if(len(sys.argv)<3):
+    train_model('/dados/rddf_predict/listen_2019-11-29_11:32:36', '/dados/log_png_1003/')    
+else:
+    train_model(sys.argv[1], sys.argv[2])
